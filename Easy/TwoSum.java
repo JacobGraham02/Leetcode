49aadfbad8ef85363dd7f767da07f9717056ad9b
Jacob Graham <jakegraham54@gmail.com>
2022-07-12 21:53:43 -0400

    Created new Java run configuration file. Created a test Java file for Easy Leetcode problem TwoSum

---
+++ Easy/TwoSum.java
@@ -0,0 +1,30 @@
+package Easy;
+
+import java.util.Map;
+import java.util.HashMap;
+
+public class TwoSum {
+
+    private int[] twoSum(int[] nums, int target){
+        Map<Integer, Integer> numsHashMap = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            int differenceBetweenTargetAndNum = target - nums[i];
+            if (numsHashMap.containsKey(differenceBetweenTargetAndNum)) {
+                return new int[] {numsHashMap.get(differenceBetweenTargetAndNum), i};
+            }
+            numsHashMap.put(nums[i], i);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        TwoSum twoSumObj = new TwoSum();
+        int nums[] = {2,7,11,15};
+        int targetNum = 9;
+
+        int resultArray[] = twoSumObj.twoSum(nums, targetNum);
+        System.out.println("[" + resultArray[0] + "," + resultArray[1] + "]");
+
+    }
+}
