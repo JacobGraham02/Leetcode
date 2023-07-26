1cfa082ec93d0a5a2304f5433d09d79eae65a89c
Jacob Graham <jakegraham54@gmail.com>
2023-07-26 15:06:26 -0400

    Changed from JUnit 4 to JUnit 5 in pom.xml, and changed the target JDK version from 1.7 to 1.17. Completed the leetcode medium problem LongestSubstringWithoutRepeatingCharacters with an associated test class

--- src/test/java/com/jacobdgraham/leetcode/medium/AddTwoNumbersTest.java
+++ src/test/java/com/jacobdgraham/leetcode/medium/AddTwoNumbersTest.java
@@ -1,8 +1,8 @@
 package com.jacobdgraham.leetcode.medium;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 final public class AddTwoNumbersTest {
 
